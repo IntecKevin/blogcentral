bb8a96bbf37deab8462c277c46e59bfd36a0a48a
IntecKevin <kevin.guidartsanchez@student.intecbrussel.be>
2021-08-16 13:31:15 +0200

    Initial commit

---
+++ src/main/java/be/intecbrussel/blogcentral/BlogcentralApplication.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.blogcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogcentralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogcentralApplication.class, args);
+    }
+
+}
