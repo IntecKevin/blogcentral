67736d18f8380b3fd0b2c7594d78016f1b878b89
Salaiaung40 <salaihtunlin@hotnail.com>
2021-08-19 18:35:48 +0200

    Add Author.java, AuthorController.java, AuthorDetailImpl.java, AuthorRepo and dependency for Bootstrap and Jquery. Add Mapping, Override and author's variables

--- src/main/java/be/intecbrussel/blogcentral/controllers/AuthorController.java
+++ src/main/java/be/intecbrussel/blogcentral/controllers/AuthorController.java
@@ -1,14 +1,46 @@
 package be.intecbrussel.blogcentral.controllers;
 
+import be.intecbrussel.blogcentral.model.Author;
+import be.intecbrussel.blogcentral.repositories.AuthorRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthorController {
 
+    @Autowired
+    private AuthorRepo repo;
 
     @GetMapping("/")
     public String viewHomePage() {
         return "index";
     }
+
+    @GetMapping("/register")
+    public String showSignUpForm(Model model) {
+        model.addAttribute("user", new Author());
+        return "register";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistering(Author author) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(author.getPassword());
+        author.setPassword(encodedPassword);
+
+        repo.save(author);
+        return "registerSuccess";
+    }
+
+    @GetMapping("/registerSuccess")
+    public String SuccessPage(Model model) {
+
+        return "registerSuccess";
+    }
+
+
 }
--- src/main/java/be/intecbrussel/blogcentral/model/Author.java
+++ src/main/java/be/intecbrussel/blogcentral/model/Author.java
@@ -1,5 +1,121 @@
 package be.intecbrussel.blogcentral.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authors")
 public class Author {
-    //test
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false, unique = true, length = 44)
+    private String email;
+
+    @Column (nullable = false, length = 64)
+    private String password;
+
+
+    @Column(nullable = false, length = 20)
+    private String firstName;
+
+
+    @Column(nullable = false, length = 20)
+    private String lastName;
+
+    @Column(nullable = false, length = 20)
+    private String userName;
+
+    @Column(nullable = false, length = 100)
+    private String streetName;
+
+    @Column(nullable = false, length = 10)
+    private int houseNo;
+
+    @Column(nullable = false, length = 20)
+    private String cityName;
+
+    @Column(nullable = false, length = 6)
+    private int zipCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public int getHouseNo() {
+        return houseNo;
+    }
+
+    public void setHouseNo(int houseNo) {
+        this.houseNo = houseNo;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
 }
